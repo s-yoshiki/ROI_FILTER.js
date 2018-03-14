34138bd96cdd6fe728822efd671e0a1722d210b1
Shinagawa Yoshiki <s.yoshiki1123@gmail.com>
2018-03-15 00:59:24 +0900

    デモ用処理の細かい修正、他

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ window.onload = function(){
 
         
         function tt() {
+            //alert()
             x++;
             cv.setImage(image, 512, 512);
             //cv.circle.threshold(50,x,0,100,100);
             //cv.rect.threshold(50,x,0,290,290);
             //cv.rect.scalar(1,0,0,x   ,30,40,40);
-            //cv.rect.scalar(0,1,0,x+20,60,40,40);
+            // cv.rect.scalar(0,1,0,x+20,60,40,40);
             //cv.rect.scalar(0,0,1,x+40,90,40,40);
             //cv.rect.change(x,0,150,300,"g","b","r");
 
@@ -28,11 +29,11 @@ window.onload = function(){
             //cv.triangle.scalar(27+x,30,100,170,300,200,0.9,0.5,1);
             //cv.triangle.sandstorm(100,50,50,100,150,100);
             //cv.diamond.sandstorm(x,150,100,150);
-            cv.rect.grayscale(x,150,100,150);
+            // cv.rect.grayscale(x,150,100,150);
             //cv.polygon.sandstorm([70,30,30,180,250],[30,80,160,160,80]);
             //cv.polygon.sandstorm([100,50,150,200],[50,100,100,50]);
             //cv.rect.grayscale(x,100,300,300);
-            //cv.threshold(100);
+            cv.threshold(100);
             //cv.circle.median(x,150,100,100);
             //cv.median();
             //cv.circle.average(x,150,100,100);
@@ -40,7 +41,6 @@ window.onload = function(){
                 x=0;
             }
         }
-        //tt();
         setInterval(tt, 10);
     }catch(e) {
         alert(e);
